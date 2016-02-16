83eeadc19382108c41fccd8555f54459985b4ab0
George Hodulik <George Hodulik>
2016-02-16 17:51:06 -0500

    Sped things up by using networkx functions

--- Naive1_0_Alg.py
+++ Naive1_0_Alg.py
@@ -2,7 +2,7 @@ from sets import Set
 import Queue as Q
 import pyodbc as odbc
 import networkx as nx
-from datetime import datetime as dt
+import timeit
 
 def main():
 	getGraphSummary("uniprotTest")
@@ -27,17 +27,19 @@ def getGraphSummary(dbname):
 		
 		for v in two_hop_neighbors:
 			suv = calcSUV(G, S, u, v)
-			print suv
 			if suv > 0:
 				H.put(-suv, (u,v))
+				#print suv
 		count += 1
-		if count % 100 == 0:
+		if count % 10000 == 0:
 			print count
+	for _ in range(0,25):		
+		print H.get()
 		
 
 #Expects original graph G, and nodes from S		
 def calcSUV(G, S, u, v):
-	beg = dt.now().time()
+	#beg = timeit.default_timer()
 	uNeighbors = set(G.neighbors(u))
 	vNeighbors = set(G.neighbors(v))
 	neighbors = uNeighbors.union(vNeighbors)
@@ -47,23 +49,23 @@ def calcSUV(G, S, u, v):
 	
 	numNodesW = len(S.node[u]['contains']) + len(S.node[v]['contains'])
 	costW = 0
-	cur = dt.now().time()
-	print cur - beg
+	#cur = timeit.default_timer()
+	#print cur - beg
 	for n in neighbors:
 		A_wn = 0
 		for uNode in S.node[u]['contains']:
-			if (uNode, n) in G.edges():
+			if G.has_edge(uNode, n):
 				A_wn += 1
 		for vNode in S.node[v]['contains']:
-			if (vNode, n) in G.edges():
+			if G.has_edge(vNode, n):
 				A_wn += 1
 		if numNodesW - A_wn + 1 < A_wn:
 			costW += numNodesW - A_wn + 1
 		else:
 			costW += A_wn
-	cur = dt.now().time()
-	print cur - beg
-	print str(costU)+" "+str(costV)+" "+str(costW)
+	#cur = timeit.default_timer()
+	#print cur - beg
+	#print str(costU)+" "+str(costV)+" "+str(costW)
 	return float(costU + costV - costW)/float(costU + costV)
 
 def getGraphFromRDFDB(dbname):
