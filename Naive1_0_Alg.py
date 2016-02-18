58ac2179cfc2a34a2b2e2f9db98b575248896bad
George Hodulik <George Hodulik>
2016-02-17 20:03:51 -0500

    Finally initialization seems to work with a reasonable runtime.

--- Naive1_0_Alg.py
+++ Naive1_0_Alg.py
@@ -15,7 +15,7 @@ def getGraphSummary(dbname):
 	print "Graph copied"
 	nodeToSupernode = {}
 	for node in S.nodes():
-		S.node[node]['cost'] = len(S.neighbors(node))
+		S.node[node]['cost'] = get_initial_cost(G,node)
 		S.node[node]['contains'] = set([node])
 		nodeToSupernode[node] = node
 	print "Graph annotated"
@@ -23,7 +23,7 @@ def getGraphSummary(dbname):
 	
 	count = 0
 	for u in S.nodes():
-		two_hop_neighbors = Set()
+		two_hop_neighbors = set()
 		for neighbors1 in G.neighbors(u):
 			for neighbors2 in G.neighbors(neighbors1):
 				two_hop_neighbors.add(neighbors2)
@@ -32,50 +32,88 @@ def getGraphSummary(dbname):
 			suv = calcSUV(G, S, u, v, nodeToSupernode)
 			if suv > 0:
 				H.put(-suv, (u,v))
-				#print suv
+				print suv
 		count += 1
 		if count % 10000 == 0:
 			print count
-	for _ in range(0,25):		
+	i = 0
+	while i in range(0,25) and len(H) > 0:		
 		print H.get()
-		
+		i += 1
+
+def get_initial_cost(G,node):
+	return len(G.predecessors(node)) + len(G.successors(node))
 
 #Expects original graph G, and nodes from S		
 def calcSUV(G, S, u, v, nodeToSupernode):
 	#beg = timeit.default_timer()
-	neighbors = set()
-	for uNode in S.node[u]['contains']:
-		for neighbor in G.neighbors(uNode):
-			neighbors.add(nodeToSupernode[neighbor])
-	for vNode in S.node[v]['contains']:
-		for neighbor in G.neighbors(vNode):
-			neighbors.add(nodeToSupernode[neighbor])
-	
+
 	costU = S.node[u]['cost']
 	costV = S.node[v]['cost']
-	
 	numNodesW = len(S.node[u]['contains']) + len(S.node[v]['contains'])
+
+	forward_neighbors = get_forward_neighbors(G, S, u, v, nodeToSupernode)
+	inward_neighbors = get_inward_neighbors(G, S, u, v, nodeToSupernode)
+	
 	costW = 0
 	#cur = timeit.default_timer()
 	#print cur - beg
-	for ns in neighbors:
+	#Calculate forward cost
+	for fn in forward_neighbors:
 		A_wn = 0
-		for n in S.node[ns]['contains']:
+		for n in S.node[fn]['contains']:
 			for uNode in S.node[u]['contains']:
 				if G.has_edge(uNode, n):
 					A_wn += 1
 			for vNode in S.node[v]['contains']:
 				if G.has_edge(vNode, n):
 					A_wn += 1
-		piWN = numNodesW * len(S.node[ns]['contains'])
-		if numNodesW - A_wn + 1 < A_wn:
-			costW += numNodesW - A_wn + 1
+		piWN = numNodesW * len(S.node[fn]['contains'])
+		if piWN - A_wn + 1 < A_wn:
+			costW += piWN - A_wn + 1
 		else:
 			costW += A_wn
+
+	for nn in forward_neighbors:
+		A_wn = 0
+		for n in S.node[nn]['contains']:
+			for uNode in S.node[u]['contains']:
+				if G.has_edge(n, uNode):
+					A_wn += 1
+			for vNode in S.node[v]['contains']:
+				if G.has_edge(n, uNode):
+					A_wn += 1
+		piWN = numNodesW * len(S.node[nn]['contains'])
+		if piWN - A_wn + 1 < A_wn:
+			costW += piWN - A_wn + 1
+		else:
+			costW += A_wn
+
+	#Calculate inward cost
 	#cur = timeit.default_timer()
 	#print cur - beg
 	#print str(costU)+" "+str(costV)+" "+str(costW)
 	return float(costU + costV - costW)/float(costU + costV)
+
+def get_forward_neighbors(G, S, u, v, nodeToSupernode):
+	forward_neighbors = set()
+	for uNode in S.node[u]['contains']:
+		for neighbor in G.successors_iter(uNode):
+			forward_neighbors.add(nodeToSupernode[neighbor])
+	for vNode in S.node[v]['contains']:
+		for neighbor in G.successors_iter(vNode):
+			forward_neighbors.add(nodeToSupernode[neighbor])
+	return forward_neighbors
+
+def get_inward_neighbors(G, S, u, v, nodeToSupernode):
+	forward_neighbors = set()
+	for uNode in S.node[u]['contains']:
+		for neighbor in G.predecessors_iter(uNode):
+			forward_neighbors.add(nodeToSupernode[neighbor])
+	for vNode in S.node[v]['contains']:
+		for neighbor in G.predecessors_iter(vNode):
+			forward_neighbors.add(nodeToSupernode[neighbor])
+	return forward_neighbors
 
 def getGraphFromRDFDB(dbname):
 	cnxn = odbc.connect(r'Driver={SQL Server};Server=.\SQLEXPRESS;Database='+dbname+r';Trusted_Connection=yes;')
